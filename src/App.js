d050375d70f38a119d1738ae22ed3b8bce6a5fd0
Votre fatima <ndeyefatoupouye28@email.com>
2026-04-23 17:44:16 +0000

    Ajout composant Statistique

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import './App.css';
 import Header from './Header';
+import Statistique from './Statistique';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="contenu">
-        <div className="statistique">
-          <h2>10 lignes</h2>
-          <p>disponibles à Dakar</p>
-        </div>
-
-        <div className="statistique">
-          <h2>150 arrêts</h2>
-          <p>dans tout Dakar</p>
-        </div>
-
-        <div className="statistique">
-          <h2>50 000 voyageurs</h2>
-          <p>par jour</p>
-        </div>
+        <Statistique />
+        <Statistique />
+        <Statistique />
       </main>
     </div>
   );
